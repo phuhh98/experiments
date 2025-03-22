9e2ce5f1ac78297a579f5f3e187a93cd17e8eac8
phu_huynh <phuhh98@gmail.com>
2025-03-22 21:35:02 +0700

    fix tailwind issue

--- experiment-amplify-ui-kit/tailwind.config.js
+++ experiment-amplify-ui-kit/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/*.{html,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
